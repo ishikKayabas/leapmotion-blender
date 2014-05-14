70770739d4da7ce94fa82299cf797059af1a9d0b
Ishik Kayabas <ishikkayabas@gmail.com>
2014-05-14 19:39:19 -0400

    Update setup.py
    Moved swig script to bld.bat and build.sh.

--- setup.py
+++ setup.py
@@ -7,7 +7,3 @@ setup(
     packages=['leapmotion-blender',],
     license='BSD',
 )
-
-swig -c++ -python -o LeapPython.cpp -interface LeapPython Leap.i
-
-g++ -fPIC -I /path/to/python3.3/include/python3.3/ LeapPython.cpp /usr/lib/Leap/libLeap.so -shared -o LeapPython.so
