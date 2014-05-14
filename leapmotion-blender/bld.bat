0d86422b8bb1ba6e6b625191ddbca74673669231
Ishik Kayabas <ishikkayabas@gmail.com>
2014-05-14 19:58:43 -0400

    Update bld.bat
    Added comments

--- leapmotion-blender/bld.bat
+++ leapmotion-blender/bld.bat
@@ -1,6 +1,11 @@
+REM Compiles the python setup script
 python setup.py install
+REM Exits the install if there's any error
 if errorlevel 1 exit 1
 
+REM Calls swig to convert LeapPython.cpp into a Python library
 swig -c++ -python -o LeapPython.cpp -interface LeapPython Leap.i
 
+REM Calls the C++ compiler to compile the original C++ file, LeapPython.cpp, to generate the binary source code,
+REM LeapPython.so, as it is also needed for Blender to use the generated Python library
 g++ -fPIC -I /path/to/python3.3/include/python3.3/ LeapPython.cpp /usr/lib/Leap/libLeap.so -shared -o LeapPython.so
