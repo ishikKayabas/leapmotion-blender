b77636dc13be097fd2955733db39fc47d7c5808c
Ishik Kayabas <ishikkayabas@gmail.com>
2014-05-14 19:22:26 -0400

    Update build.sh
    Added Swig scripting.

--- leapmotion-blender/build.sh
+++ leapmotion-blender/build.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+swig -c++ -python -o LeapPython.cpp -interface LeapPython Leap.i
+
+g++ -fPIC -I /path/to/python3.3/include/python3.3/ LeapPython.cpp /usr/lib/Leap/libLeap.so -shared -o LeapPython.so
