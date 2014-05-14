f32bf8dc08b6abea4dcac074960068d5a66f3638
Ishik Kayabas <ishikkayabas@gmail.com>
2014-05-14 19:22:06 -0400

    Update bld.bat
    Added swig script, might need altering for Windows system functionality.

--- leapmotion-blender/bld.bat
+++ leapmotion-blender/bld.bat
@@ -1,2 +1,6 @@
 python setup.py install
 if errorlevel 1 exit 1
+
+swig -c++ -python -o LeapPython.cpp -interface LeapPython Leap.i
+
+g++ -fPIC -I /path/to/python3.3/include/python3.3/ LeapPython.cpp /usr/lib/Leap/libLeap.so -shared -o LeapPython.so
